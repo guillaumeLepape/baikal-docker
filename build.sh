bcb2f1cbfde694d8d7ce402e213bc9ca59505185
guillaumeLepape <63458567+guillaumeLepape@users.noreply.github.com>
2026-08-22 15:04:20 +0200

    Putting the right username (#1)

--- build.sh
+++ build.sh
@@ -5,4 +5,4 @@ set -e
 BAIKAL_VERSION=0.12.1
 BUILD_VERSION=0
 
-docker buildx build --build-arg BAIKAL_VERSION=$BAIKAL_VERSION -t glepape/baikal:${BAIKAL_VERSION}.${BUILD_VERSION} .
+docker buildx build --build-arg BAIKAL_VERSION=$BAIKAL_VERSION -t guillaumelepape/baikal:${BAIKAL_VERSION}.${BUILD_VERSION} -t guillaumelepape/baikal:latest .
