ef2e52bd4e65485744d6a1b1a5b52cff0f3f077a
Guillaume Le Pape <gui.lepape25@gmail.com>
2026-08-22 19:00:00 +0200

    Add release workflow

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -e
-
-BUILD_VERSION=0
-
-BAIKAL_VERSION=$(grep -m1 '^ARG BAIKAL_VERSION=' Dockerfile | cut -d= -f2)
-
-docker buildx build --build-arg BAIKAL_VERSION=$BAIKAL_VERSION -t guillaumelepape/baikal:${BAIKAL_VERSION}.${BUILD_VERSION} -t guillaumelepape/baikal:latest .
