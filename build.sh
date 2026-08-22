4918c55b34de3c3a415458f7b3b97484f5d82452
Guillaume Le Pape <gui.lepape25@gmail.com>
2026-08-22 15:29:29 +0200

    Build image on pull request for testing

--- build.sh
+++ build.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-BAIKAL_VERSION=0.12.1
 BUILD_VERSION=0
+
+BAIKAL_VERSION=$(grep -m1 '^ARG BAIKAL_VERSION=' Dockerfile | cut -d= -f2)
 
 docker buildx build --build-arg BAIKAL_VERSION=$BAIKAL_VERSION -t guillaumelepape/baikal:${BAIKAL_VERSION}.${BUILD_VERSION} -t guillaumelepape/baikal:latest .
